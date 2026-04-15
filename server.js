70fec0985ba725b30a8eb00fe5940d6f55661ea9
guilleriveraa <guilleriveraa12@gmail.com>
2026-04-15 10:44:07 +0200

    correo2

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ console.log('✅ Nodemailer cargado');
 require('dotenv').config();
 // ===== CONFIGURACIÓN DE NODEMAILER =====
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true para SSL (puerto 465)
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
